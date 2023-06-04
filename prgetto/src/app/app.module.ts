2f240e0301080650d929d6d0f230b33fe6422eb7
DanielPacc <98812974+DanielPacc@users.noreply.github.com>
2023-06-04 17:28:02 +0200

    Update app.module.ts

--- prgetto/src/app/app.module.ts
+++ prgetto/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule], // Aggiungi FormsModule ai moduli importati
+  imports: [BrowserModule, FormsModule, HttpClientModule], // Aggiungi FormsModule ai moduli importati
   providers: [],
   bootstrap: [AppComponent]
 })
